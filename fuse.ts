554c1e152459b4797fda446ed56b81c5be0ab1d6
hwangtan <hwangwonjun@gmail.com>
2019-07-11 02:37:13 +0900

    Wrapping Sparky

--- fuse.ts
+++ fuse.ts
@@ -1,41 +1,41 @@
-import { FuseBox, WebIndexPlugin } from "fuse-box";
+import { FuseBox, WebIndexPlugin, Sparky } from "fuse-box";
 
-const fuse = FuseBox.init({
-  homeDir: "src",
-  output: "dist/$name.js",
-  plugins: [
-    WebIndexPlugin({
-      title: "My awesome website",
-      bundles: ["client/app"],
-      template: "static/index.html",
-    }),
-  ],
-  hash: true,
-  useTypescriptCompiler: true,
-  log: {
-    showBundledFiles: false,
-    clearTerminalOnBundle: true,
-  },
-  // shim: {
-  //     jquery: {
-  //         source: "node_modules/jquery/dist/jquery.js",
-  //         exports: "$",
-  //     },
-  // },
+Sparky.task("clean", () => Sparky.src("dist").clean("dist"));
+
+Sparky.task("default", ["clean"], () => {
+  const fuse = FuseBox.init({
+    homeDir: "src",
+    output: "dist/$name.js",
+    plugins: [
+      WebIndexPlugin({
+        title: "My awesome website",
+        bundles: ["client/app"],
+        template: "static/index.html",
+      }),
+    ],
+    hash: true,
+    useTypescriptCompiler: true,
+    log: {
+      showBundledFiles: false,
+      clearTerminalOnBundle: true,
+    },
+  });
+
+  fuse
+    .bundle("server/app")
+    .target("server@esnext")
+    .watch("server/**")
+    .instructions(" > [server/main.ts]")
+    .completed(proc => proc.start());
+
+  fuse
+    .bundle("client/app")
+    .target("browser@esnext")
+    .watch("client/**")
+    .hmr()
+    .instructions(" > client/app.tsx");
+
+  fuse.run();
 });
 
-fuse
-  .bundle("server/app")
-  .target("server@esnext")
-  .watch("server/**")
-  .instructions(" > [server/main.ts]")
-  .completed(proc => proc.start());
-
-fuse
-  .bundle("client/app")
-  .target("browser@esnext")
-  .watch("client/**")
-  .hmr()
-  .instructions(" > client/app.tsx");
-
-fuse.run();
+Sparky.start("default");
